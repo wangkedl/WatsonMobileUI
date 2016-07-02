6ef825250cf33f10f2e74e883bb1037a7ba855e3
ibmuser <ibmuser@lixingjiembp.lan>
2016-07-02 16:01:15 +0800

    update

--- WatsonMobileUI/ViewController.swift
+++ WatsonMobileUI/ViewController.swift
@@ -14,34 +14,67 @@ class ViewController: UIViewController, ChatDataSource,UITextFieldDelegate {
         setupSendPanel()
     }
     
+    override func viewWillAppear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification:NSNotification){
+        
+        
+        if ((notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue()) != nil {
+        //let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height, right: 0)
+        //let keyboardheight  = keyboardSize.height as CGFloat
+        //let frame = self.nameInput.frame
+        // frame.origin.y = frame.origin.y - keyboardheight
+        //var offset = 156 as CGFloat
+            
+        let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue()
+        let keyboardheight  = keyboardSize!.height as CGFloat
+
+        
+        let width = self.view.frame.size.width;
+        let height = self.view.frame.size.height;
+        let rect = CGRectMake(0.0, -keyboardheight,width,height);
+        self.view.frame = rect
+        }
+    }
+    
+    func keyboardWillHide(notification:NSNotification){
+        let width = self.view.frame.size.width;
+        let height = self.view.frame.size.height;
+        let rect = CGRectMake(0.0, 0,width,height);
+        self.view.frame = rect
+    }
+    
     
     func setupSendPanel()
     {
         let screenWidth = UIScreen.mainScreen().bounds.width
-        let sendView = UIView(frame:CGRectMake(0,self.view.frame.size.height - 56,screenWidth,56))
+        let sendView = UIView(frame:CGRectMake(0,self.view.frame.size.height - 50,screenWidth,50))
         
         sendView.backgroundColor = UIColor(red:0, green:0.1, blue:0.1, alpha:0.1)
-        sendView.layer.cornerRadius = 6.0
+        sendView.alpha = 0.5
         
-        txtMsg = UITextField(frame:CGRectMake(42,10,screenWidth - 95,36))
+        txtMsg = UITextField(frame:CGRectMake(44,7,screenWidth - 95,36))
         txtMsg.backgroundColor = UIColor.whiteColor()
         txtMsg.textColor = UIColor.blackColor()
         txtMsg.font = UIFont.boldSystemFontOfSize(12)
         txtMsg.clearButtonMode = UITextFieldViewMode.WhileEditing
         txtMsg.keyboardType = UIKeyboardType.ASCIICapable
-        txtMsg.layer.cornerRadius = 10.0
+        txtMsg.layer.cornerRadius = 5.0
         txtMsg.returnKeyType = UIReturnKeyType.Send
         txtMsg.delegate = self
         sendView.addSubview(txtMsg)
         self.view.addSubview(sendView)
         
-        let mircoButton = UIButton(frame:CGRectMake(5,10,40,35))
+        let mircoButton = UIButton(frame:CGRectMake(5,8,40,35))
         mircoButton.addTarget(self, action:#selector(ViewController.sendMessage) ,
                             forControlEvents:UIControlEvents.TouchUpInside)
         mircoButton.setImage(UIImage(named:"mirco2"),forState:UIControlState.Normal)
         sendView.addSubview(mircoButton)
         
-        let addButton = UIButton(frame:CGRectMake(screenWidth - 45,12,33,30))
+        let addButton = UIButton(frame:CGRectMake(screenWidth - 45,9,33,30))
         addButton.addTarget(self, action:#selector(ViewController.sendMessage) ,
                              forControlEvents:UIControlEvents.TouchUpInside)
         addButton.setImage(UIImage(named:"add3"),forState:UIControlState.Normal)
@@ -82,14 +115,14 @@ class ViewController: UIViewController, ChatDataSource,UITextFieldDelegate {
     
     func setupChatTable()
     {
-        self.tableView = TableView(frame:CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height - 75), style: .Plain)
+        self.tableView = TableView(frame:CGRectMake(0, 20, self.view.frame.size.width, self.view.frame.size.height - 70), style: .Plain)
         
         // 创建一个重用的单元格
         self.tableView!.registerClass(TableViewCell.self, forCellReuseIdentifier: "ChatCell")
-        me = UserInfo(name:"Xiaoming" ,logo:("xiaohua.png"))
-        Watson  = UserInfo(name:"Xiaohua", logo:("earth.png"))
+        me = UserInfo(name:"user" ,logo:("femail.png"))
+        Watson  = UserInfo(name:"watson", logo:("rainbow.png"))
         
-        let zero =  MessageItem(body:"Hi Dear,What can I do for you!", user:Watson,  date:NSDate(timeIntervalSinceNow:0), mtype:ChatType.Someone)
+        let zero =  MessageItem(body:"Hi Dear,I'm watson,What can I do for you!", user:Watson,  date:NSDate(timeIntervalSinceNow:0), mtype:ChatType.Someone)
         
         Chats = NSMutableArray()
         Chats.addObjectsFromArray([zero])
